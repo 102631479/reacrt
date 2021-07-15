bb38eb7459e0fabd961fd58129c2b7f8ea147f89
102631479 <78398324+102631479@users.noreply.github.com>
2021-07-15 14:00:28 +0800

    开始界面

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -6,3 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+return React.createElement('div', {className: 'shopping-list'},
+  React.createElement('h1', /* ... h1 children ... */),
+  React.createElement('ul', /* ... ul children ... */)
+);
