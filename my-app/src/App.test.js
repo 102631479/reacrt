aa476bcac3488b4daf3fe17130f67960b3f83cae
102631479 <78398324+102631479@users.noreply.github.com>
2021-07-15 17:59:11 +0800

    加入ant框架

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -7,7 +7,3 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-return React.createElement('div', {className: 'shopping-list'},
-  React.createElement('h1', /* ... h1 children ... */),
-  React.createElement('ul', /* ... ul children ... */)
-);
