aa3708ba314bf954fa61c225e4af3a17dc1fb15c
丁宁 <78398324+102631479@users.noreply.github.com>
2023-05-31 17:30:52 +0800

    dn

--- react/src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(proxy('/ms', { target: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',changeOrigin: true }));
-};
--- react18_Ts/hello.ts
+++
@@ -1,5 +0,0 @@
-const Hello = (name: string) => {
-    return `hello ${name}`
-}
-
-Hello('666啊')
